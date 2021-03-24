8c9bff412f0f0ffa3691779e8711b7ceb699f53e
Tripp.Lamb <trippalamb@gmail.com>
2021-03-23 22:09:01 -0500

    started adding randomization

--- src/app/canvas.js
+++ src/app/canvas.js
@@ -1,34 +1,72 @@
 class Sun{
-    constructor(color, minRadius, maxRadius, minX, maxX, minY,  maxY){
+    constructor(color, minRadius, maxRadius, minX, maxX, minY,  maxY, rMargin){
         this.color = color;
-        this.r = 120;
+        this.r = Math.random()*(maxRadius - minRadius) + minRadius;
+        let margin = this.r*rMargin;
+        this.x = Math.random()*(maxX - minX - margin*2.0) + minX + margin;
+        this.y = Math.random()*(maxY - minY - margin*2.0) + minY + margin;
 
     }
 
     draw(svg){
         svg.append("circle")
-            .attr('cx', 150)
-            .attr('cy', 220)
+            .attr('cx', this.x)
+            .attr('cy', this.y)
             .attr('r', this.r)
             .style('fill', this.color);
     }
 }
 
 class MountainRange{
-    constructor(color, width, baseY, lateralVariation, minPoints, maxPoints, minSubdiv, maxSubdiv){
+    constructor(color, width, baseY, maxDeltaY, lateralVariation, minPoints, maxPoints, minSubdiv, maxSubdiv){
         this.color = color;
         this.width = width;
         this.baseY = baseY;
+        this.maxDeltaY = maxDeltaY;
+        this.lateralVariation = lateralVariation;
+        this.minPoints = minPoints;
+        this.maxPoints = maxPoints;
+        this.dp = maxPoints - minPoints;
+        this.n = Math.ceil(Math.random() * this.dp) + minPoints + 2;
+        this.minSubdic = minSubdiv;
+        this.maxSubdiv = maxSubdiv;
+
+        this.buildRange();
+
 
     }
 
     draw(svg, height){
-        svg.append("rect")
-            .attr("x", 0)
-            .attr("y", this.baseY)
-            .attr("width", this.width)
-            .attr("height", height-this.baseY)
+
+        let path = new d3.path();
+        path.moveTo(this.points[0].x, this.points[0].y);
+        for(let i = 1; i < this.points.length; i++){
+            path.lineTo(this.points[i].x, this.points[i].y);
+        }
+        path.lineTo(this.width, height);
+        path.lineTo(0, height);
+        path.closePath();
+
+        svg.append("path")
+            .attr("d", path)
             .attr("fill", this.color);
+
+    }
+
+    buildRange(){
+
+        this.points = [];
+        let sign = Math.sign(Math.random()-0.5);
+        
+        for(let i = 0; i<=this.n;i++){
+            let dx = this.width/(this.n-1);
+            let x = (i === 0 || i === this.n) ? (dx*i) : (dx*i) + (dx*this.lateralVariation);
+            let y = sign*Math.random()*this.maxDeltaY + this.baseY;
+            sign*=-1;
+            this.points.push({x:x,y:y});
+        }
+
+
     }
 }
 
@@ -43,13 +81,14 @@ class Canvas{
         this.colors = ["#ede6d6", "#f9c792", "#85aeaa", "#388199", "#323232"]
 
         //sun config
-        this.radius = [60, 120];
-        this.sunAllowable = 0.4;
+        this.radius = [80, 160];
+        this.sunAllowable = 0.6;
 
         //mountains config
-        this.ys = [200, 350, 450];
-        this.latVar = [0, 0, 0];
-        this.nPoints = [[0, 0],[0, 0],[0, 0]];
+        this.ys = [300, 400, 500];
+        this.maxDeltaYs = [120,40,40]
+        this.latVar = [0.4, 0.2, 0.2];
+        this.nPoints = [[1, 1],[3, 3],[3, 3]];
         this.nSubdiv = [[0, 0],[0, 0],[0, 0]];
 
 
@@ -61,7 +100,8 @@ class Canvas{
             0, 
             this.width, 
             0, 
-            (this.height*this.sunAllowable)
+            (this.height*this.sunAllowable),
+            0.3
         );
 
         this.mountains = [];
@@ -70,7 +110,8 @@ class Canvas{
                 this.colors[i+2], 
                 this.width, 
                 this.ys[i], 
-                this.latVar[i], 
+                this.maxDeltaYs[i],
+                this.latVar[i],
                 this.nPoints[i][0], 
                 this.nPoints[i][1], 
                 this.nSubdiv[i][0], 
@@ -98,3 +139,4 @@ class Canvas{
     }
 
 }
+
